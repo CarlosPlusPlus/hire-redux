4c9901ec6fe7b10a13f43bec735c752ede13d266
cjlwired <carlos.jose.lazo@gmail.com>
2013-11-29 18:20:41 -0500

    Add in new logic for Secret Token for session persistence.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HireCrm::Application.config.secret_token = '697889afa6d6356293e956c56c13695aaa3cd9c10fcfa9b20ef0f7290dfc40c779a8abc62e53d9c53141a5896e5b330735d994a869ed0f6048ae61ddc84960e4'
+
+HireCrm::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
